37ac9fe634ee2ba3200bc9a4c812b72eae8ab76f
Benjamin Peng <benjamin.peng@duke.edu>
2023-03-15 18:58:38 -0700

    finish date query

--- backend/index.js
+++ backend/index.js
@@ -18,34 +18,74 @@ app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
 app.use(express.json());
 
 const purgeOlderThanWeek = () => {
-  pool.query('DELETE FROM logs WHERE time < NOW() - MAKE_INTERVAL(days => 7);')//now - 7
+  pool.query('DELETE FROM logs WHERE date < CURRENT_DATE - MAKE_INTERVAL(days => 7);')//now - 7
 };
 
 //launch puppeteer
 const updateData = () => {
-  puppeteer.launch().then(async function(browser) {
+  puppeteer.launch({
+    executablePath: '/opt/homebrew/bin/chromium'
+  }).then(async function(browser) {
       const page = await browser.newPage();
       await page.goto('https://myrec.recreation.duke.edu/FacilityOccupancy');
       await page.waitForSelector('.occupancy-count');
       const element = await page.$('.occupancy-count');
+      console.log("CRON");
       const occupancy = parseInt(await page.evaluate(el => el.innerText, element));
-      pool.query(`INSERT INTO logs (time, count) VALUES (NOW(), ${occupancy});`);
+      //truncated to the nearest min
+      pool.query(`INSERT INTO logs (count, date, time) VALUES (${occupancy}, CURRENT_DATE, DATE_TRUNC('minute', NOW())::time);`);
       await browser.close();
   });
   purgeOlderThanWeek();
 };
 
+//returns the lowest occupancy time in interval
+//start, end are two element arrays [day of week, time]
+//time in format XX:XX
+const bestTimeInInterval = (start, end) => { 
+
+  pool.query('SELECT * '
+  + 'FROM logs '
+  + 'WHERE count = (SELECT MIN(count) FROM logs)'
+  + `AND time BETWEEN '${start[1]}'::time AND '${end[1]}'::time;`
+  + `AND date BETWEEN `)
+    .then((res) => console.log(res.rows[0].time + " " + res.rows[0].count));
+};
+
 //setInterval(updateData, 5000);
 
 const job = new CronJob(
-  '*/30 6-22 * * *', //every 30 min from 6am to 10pm
+  '*/1 6-22 * * *', //every 30 min from 6am to 10pm
   updateData,
   null,
   true,
-  'America/Los_Angeles'
+  'America/New_York'
 );
 
+//day = int: 0=sun, 1=mon, 2=tues, etc
+//returns in yyyy-mm-dd string
+const getLastDay = (day) => {
+  const date = new Date();
+  const currDay = date.getDay();
+
+  if (currDay > day) {
+    date.setDate(date.getDate() - date.getDay() + day);
+    return date.getFullYear() + '-' + ;
+  }
+  date.setDate(date.getDate() - date.getDay() - 7 + day);
+  return date.toString();
+}
+console.log(getLastDay(0));
+/*
+const date = new Date();
+date.setDate(date.getDate() - date.getDay());
+console.log(date.toString());
+date.setDate(date.getDate() - 7);
+console.log(date.toString());
+*/
+//bestTimeInInterval(Date.now(), Date.now());
 job.start();
+//purgeOlderThanWeek();
 
 app.get("/", function (req, res) {
   res.status(200).json({
